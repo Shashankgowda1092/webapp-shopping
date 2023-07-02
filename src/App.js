6b8d9d0c04991e00d12fd021355b5a47cd955391
unknown <shashankh1092@gmail.com>
2023-07-02 18:11:13 +0530

    all setup to start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1 className='text-6xl font-bold underline text-red-500' >tailwindcss</h1>
         <h1>shash and bhoom </h1>
         <p>
           lets build a app with react together #loveu
